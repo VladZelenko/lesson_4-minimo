aef26fd5f712de8ac220a420a05eb8a0db68185a
VladZelenko <Vlad6555@ukr.net>
2017-11-28 19:47:37 +0200

    add style header+intro section

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,3 +1,14 @@
+//load more button
+$(document).ready(function () {
+    size_li = $("#myList div").size();
+    x=2;
+    $('#myList div:lt('+x+')').show();
+    $('#loadMore').click(function () {
+        x= (x+2 <= size_li) ? x+2 : size_li;
+        $('#myList div:lt('+x+')').show();
+    });
+});
+
 $(document).ready(function(){
 	$('.parallax').parallax();
 });
